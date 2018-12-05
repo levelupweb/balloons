4ec29b43df9a62d6814dd4f2ac665c4919e93dad
kv9991 <beatzhitta@gmail.com>
2018-12-05 13:32:21 +0300

    chore: button, input, textarea

--- components/Button/consts.js
+++
@@ -1,8 +0,0 @@
-export const BUTTON_VARIANT_PRIMARY = "primary";
-export const BUTTON_VARIANT_DEFAULT = "default";
-export const BUTTON_VARIANT_SUCCESS = "success";
-export const BUTTON_VARIANT_DANGER = "danger";
-
-export const BUTTON_SIZE_SMALL = "small";
-export const BUTTON_SIZE_DEFAULT = "default";
-export const BUTTON_SIZE_BIG = "big";

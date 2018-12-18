2bc2831f7145ce88d2831c747572af7cb974abc3
Ivan K <web.ivan@iMac-Vlad.local>
2018-12-18 13:43:42 +0300

    feat(Calculator): add default logo

--- utils/getStorageUrl.js
+++ utils/getStorageUrl.js
@@ -2,6 +2,11 @@ import { getEnvConfig } from "@utils";
 
 const envConfig = getEnvConfig();
 
-export const getStorageUrl = key => `${envConfig.url}/storage/${key}`;
+export const getStorageUrl = key => {
+	if (key.indexOf("http") === -1) {
+		return `${envConfig.url}/storage/${key}`;
+	}
+	return key;
+};
 
 export default getStorageUrl;
